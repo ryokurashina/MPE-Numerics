75a816eb6e50930c67f20be756642ce04fe0cf43
Ryo Kurashina <rk2014@ic.ac.uk>
2018-10-11 22:36:01 +0100

    renamed main file and added CTCS scheme

--- linAdSchemes.py
+++ linAdSchemes.py
@@ -1,6 +1,5 @@
 """""
-Contains various numerical schemes for the linear advection equation and a
-function that returns the exact solution.
+Contains various numerical schemes for the linear advection equation.
 
 List of numerical schemes:
 * FTCS
@@ -15,10 +14,25 @@ def FTCS(phi, ntime, c):
     # Initialise for loop
     phi_old = np.array(phi)
     phi_new = np.zeros(N)
-    for i in range(0, ntime-1):
-        phi_new[1:N-1] = phi_old[1:N-1] -c/2*(phi_old[2:N]-phi_old[0:N-2])
+    for i in range(ntime):
+        phi_new[1:N-1] = phi_old[1:N-1]-c/2*(phi_old[2:N]-phi_old[0:N-2])
         # Periodic B.C.
         phi_new[0] = phi_old[0]-c/2*(phi_old[1]-phi_old[N-1])
         phi_new[N-1] = phi_new[0]
         phi_old = phi_new.copy()
+    return phi_new
+
+def CTCS(phi, ntime, c):
+    N = len(phi)
+    # Initialise for loop
+    phi_old = FTCS(phi, 1, c)
+    phi_v_old = np.array(phi)
+    phi_new = np.zeros(N)
+    for i in range(ntime-1):
+        phi_new[1:N-1] = phi_v_old[1:N-1]-c/2*(phi_old[2:N]-phi_old[0:N-2])
+        # Periodic B.C.
+        phi_new[0] = phi_v_old[0]-c/2*(phi_old[1]-phi_old[N-1])
+        phi_new[N-1] = phi_new[0]
+        phi_old = phi_new.copy()
+        phi_v_old = phi_old.copy()
     return phi_new
---
+++ main.py
@@ -0,0 +1,47 @@
+"""""
+Script for post processing of data
+"""""
+
+# Import packages and libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from linAdSchemes import *
+from initialConditions import *
+
+def main():
+    # Set resolution
+    N = 1001
+    x = np.linspace(0, 1, N)
+    a = .25
+    b = .5
+    # Initial condition
+    phi_init = squareWave(x, a, b)
+    # Initialise timestep, grid-size and advection velocity
+    dx = 1/(N-1)
+    dt = 1e-4
+    n_steps = 1000
+    u = .1
+    T = n_steps*dt
+    print((a+u*T) % 1)
+    print((b+u*T) % 1)
+    phi_exact = squareWave(x, (a+u*T) % 1, (b+u*T) % 1)
+    # Courant number
+    c = u*dt/dx
+    print("Courant Number (c): %f" %(c))
+
+    """""
+    Pick the numerical scheme here:
+    * FTCS (Linear Advection)
+    * CTCS (Linear Advection)
+    """""
+
+    # FTCS
+    phi1 = FTCS(phi_init,n_steps,c)
+    # CTCS
+    phi2 = CTCS(phi_init,n_steps,c)
+
+    plt.plot(x,phi1)
+    plt.plot(x,phi2)
+    # plt.plot(x,phi_exact)
+    plt.show()
+main()
